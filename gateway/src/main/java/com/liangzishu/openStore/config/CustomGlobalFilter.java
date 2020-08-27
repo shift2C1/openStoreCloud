a411ea1e0662c16e86285999c6b5c91116fecf7f
wangchao <shift2C1@163.com>
2020-08-27 21:17:16 +0800

    网关路由路径成功

--- gateway/src/main/java/com/liangzishu/openStore/config/CustomGlobalFilter.java
+++ gateway/src/main/java/com/liangzishu/openStore/config/CustomGlobalFilter.java
@@ -3,11 +3,19 @@ package com.liangzishu.openStore.config;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.route.Route;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.i18n.LocaleContext;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.codec.multipart.Part;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
 import java.util.Map;
 
 import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR;
@@ -15,11 +23,16 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.G
 public class CustomGlobalFilter implements GlobalFilter, Ordered {
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 //        System.out.println("自定义全局过滤器");
-        Route route = exchange.getAttribute(GATEWAY_ROUTE_ATTR);
-// get all metadata properties
-        Map<String, Object> metadata = route.getMetadata();
-// get a single metadata property
-//        route.getMetadata(someKey);
+        ServerHttpRequest request = exchange.getRequest();
+        ServerHttpResponse response = exchange.getResponse();
+        ApplicationContext applicationContext = exchange.getApplicationContext();
+        Map<String, Object> attributeMap = exchange.getAttributes();
+        Mono<MultiValueMap<String, String>> formData = exchange.getFormData();
+        LocaleContext localeContext = exchange.getLocaleContext();
+        Mono<MultiValueMap<String, Part>> multipartData = exchange.getMultipartData();
+        Mono<Principal> principal = exchange.getPrincipal();
+        Mono<WebSession> session = exchange.getSession();
+        ServerWebExchange.Builder mutate = exchange.mutate();
         return chain.filter(exchange);
     }
 
